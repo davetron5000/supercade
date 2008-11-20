e6299f10e03d4d4d914f6d86296d5d3613cbbfeb
David Copeland <davetron5000@gmail.com>
2008-11-20 17:37:54 -0500

    added without passwords

---
+++ php_includes/include.php
@@ -0,0 +1,14 @@
+<?php
+require "Band.php";
+require "News.php";
+require "Picture.php";
+require "Show.php";
+require "Showwith.php";
+require "Venue.php";
+
+$host = "localhost";
+$database = "foo";
+$user = "bar";
+$password = "baz";
+
+?>
